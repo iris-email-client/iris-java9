8a26cc5c57cb5fc9dad6066a980b94a96b6583dc
pedro <pedro@canario>
2016-07-25 11:59:35 -0300

    updating ant build script

--- src/iris.search.ui.gui/module-info.java
+++ src/iris.search.ui.gui/module-info.java
@@ -1,6 +1,6 @@
 module iris.search.ui.gui {
 	requires iris.core;
-	requires iris.tag.api;	
+	requires iris.search.api;	
 	requires iris.ui.gui;	
 	
 	provides br.unb.cic.iris.command.MailCommand with br.unb.cic.iris.search.ui.gui.command.SearchGuiCommand;
